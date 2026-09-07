cc9aac448e0e408020aad548e1a59d551f0e38a9
Abhishek Kumar <7abhik@gmail.com>
2026-09-07 19:31:51 +0400

    added filter for loading genres list

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -8,9 +8,7 @@ const GameGrid = () => {
   const { data, error, isLoading } = useGame();
   return (
     <>
-      <Text>Is loadding {isLoading + "ttt"}</Text>
       {error && <Text>{error}</Text>}
-
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
